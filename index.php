d7f53dd40bb0a3622dd5f9c8ad7e3e5221448131
Paulo Henrique Bento dos Santos <phbento@outlook.com.br>
2021-08-19 22:34:38 -0300

    Exemplo de php na primeira aula

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+ 
+ //Comentario de linha única
+
+ /*
+ comentário de multipla linha
+ */
+
+ $nome = 'Paulo';
+
+ echo 'Olá, ' . $nome. '!<br><br>'; //Concatenado
+
+ echo "Olá, $nome"; //Não concatenado
+
+ if( $nome == 'Paulo') {
+    echo '<br><br> O nome é igual';
+ }else{
+     echo '<br><br> Nome diferente'
+ }
+
+ for ( $i = 0; $i < 10; $i++){
+     echo "Linha $i <br>"
+ }
+
+ echo '<br> 2 + 2 =' . (2 +2) . '!';
