c848425fd013e049c6c2496edfd29a85fa68c9de
Paulo Henrique Bento dos Santos <phbento@outlook.com.br>
2021-09-02 22:37:13 -0300

    Final da aula - Vetor, matriz, formulario

--- vetor.php
+++ vetor.php
@@ -36,3 +36,40 @@ $semana['Sabado']           = 'Aula de ingles na Cultura Inglesa';
 foreach ( $semana as $dia => $atividade ) {
     echo "No dia $dia eu faço $atividade <br>";
 }
+
+echo "<br>";/*
+$disciplina['PI'] = 'segunda';
+$disciplina['CMS'] = 'terca';
+$disciplina['DD'] = 'terca';
+$disciplina['BB'] = 'quarta';
+$disciplina['LS'] = 'quinta';
+$disciplina['Script'] = 'sexta';
+
+
+foreach ( $disciplina as $aula => $dia){
+    echo "A aula de $aula é: $dia <br>";
+}*/
+
+
+$disciplina['Segunda'][0] = "Pi";
+$disciplina['Terca'][0] = "CMS";
+$disciplina['Terca'][1] = "Direito";
+$disciplina['Quarta'][0] = "Banco";
+$disciplina['Quinta'][0] = "PhP";
+$disciplina['Sexta'][0] = "JavaScript";
+
+echo "<br>";
+
+foreach( $disciplina as $dia => $disciplina){
+
+    echo "Aula(s) na $dia ";
+
+    foreach( $disciplina as $disciplina ){
+
+        echo "$disciplina, ";
+    }
+
+    echo "<br>";
+}
+
+include('linkform.html');
